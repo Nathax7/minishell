c2d3da8861a98170e1d9de3825f6dbe7a4a592af
Alexandre Meekel-Konjikovic <almeekel@z4r2p5.42lyon.fr>
2025-04-09 17:22:36 +0200

    tokenizer

---
+++ src/parsing/tokenizer.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokenizer.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: almeekel <almeekel@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/09 17:14:42 by almeekel          #+#    #+#             */
+/*   Updated: 2025/04/09 17:22:17 by almeekel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+skip_spaces(char *str, int *i)
+{
+
+
+	
+}
+
+// find |, <, >, >>, << //
+identify_operator(char *str, int *i)
+{}
+// finds words //
+identify_word(char *str, int *i)
+{
+	
+
+}	
+
+t_token	*tokenize(char *input)
+{
+	
+}
+
