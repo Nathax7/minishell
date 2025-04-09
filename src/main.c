1d16ed55ded5cbdb9813972f5703d9d696568219
Alexandre Meekel-Konjikovic <almeekel@z4r2p5.42lyon.fr>
2025-04-09 17:27:53 +0200

    main example

--- src/main.c
+++ src/main.c
@@ -6,7 +6,46 @@
 /*   By: almeekel <almeekel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 17:13:51 by almeekel          #+#    #+#             */
-/*   Updated: 2025/04/09 17:13:52 by almeekel         ###   ########.fr       */
+/*   Updated: 2025/04/09 17:26:46 by almeekel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/minishell.h"
+
+void	shell_loop(void)
+{
+	char	*input;
+	t_token	*tokens;
+
+	while (1)
+	{
+		input = readline("minishell$ ");
+		if (!input) {
+			printf("exit\n");
+			break;
+		}
+		if (*input)
+			add_history(input);
+
+		// Tokenize
+		tokens = tokenize(input);
+		print_tokens(tokens);
+
+		// Envoi vers parsing -> execution (à faire)
+		// t_command *cmd = parse_tokens(tokens);
+		// exec_command(cmd);
+
+		free_tokens(tokens);
+		free(input);
+	}
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+
+	shell_loop();
+	return (0);
+}
--- src/parsing/parsing_utils.c
+++ src/parsing/parsing_utils.c
@@ -1,45 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tokenizer.c                                        :+:      :+:    :+:   */
+/*   parsing_utils.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: almeekel <almeekel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/09 17:14:42 by almeekel          #+#    #+#             */
-/*   Updated: 2025/04/09 17:24:44 by almeekel         ###   ########.fr       */
+/*   Created: 2025/04/09 17:26:06 by almeekel          #+#    #+#             */
+/*   Updated: 2025/04/09 17:26:08 by almeekel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-skip_spaces(char *str, int *i)
-{
-
-
-	
-}
-
-// find |, <, >, >>, << //
-identify_operator(char *str, int *i)
-{
-	if (str[*i]) == '|'
-	if (str[*i]) == '|'
-	if (str[*i]) == '|'
-	if (str[*i]) == '|'
-
-
-	
-}
-// finds words //
-identify_word(char *str, int *i)
-{
-	
-
-}
-//adds the token to the list //
-add_token(t_token **head, char *val, t_token_type type)
-{}
-
-t_token	*tokenize(char *input)
-{
-	
-}
 
