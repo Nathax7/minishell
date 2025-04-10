063d31e84e1874f1a36ea57155fd81ed2797b351
Alexandre Meekel-Konjikovic <almeekel@z4r2p5.42lyon.fr>
2025-04-10 12:32:36 +0200

    makef

---
+++ inc/parsing.h
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nagaudey <nagaudey@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/09 16:44:00 by almeekel          #+#    #+#             */
+/*   Updated: 2025/04/09 22:03:35 by nagaudey         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSING_H
+# define PARSING_H
+
+# include "../libft/all.h"
+# include <errno.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+# define CHARSET "abcdefghijklmnopqrstuvwxyz"
+
+typedef enum
+{
+	NO_HEREDOC,
+	HEREDOC
+}	t_heredoc;
+
+typedef enum
+{
+	NO_ENV,
+	ENV
+}	t_env;
+
+typedef struct s_cmd
+{
+	t_heredoc	here_doc;
+	t_env		env;
+	pid_t	*pids;
+	char	*cmd;
+	char	**cmd_args;
+	char	**paths;
+	char	*path;
+	int		infile;
+	int		outfile;
+	char	*infile_name;
+	char	*outfile_name;
+	int		fd[2];
+	int		status;
+}	t_cmd;
+
+void	minishell_init(t_cmd *cmd, char **envp);
+void	free_parent(t_cmd *cmd, int status, char *str, char *str2);
+void	msg_cmd(char *str, char *str2);
+void	is_here_doc(t_cmd *cmd, char **av, int ac);
+void	open_infile(t_cmd *cmd, char *infile);
+void	open_outfile(t_cmd *cmd, char *outfile, int mode);
+void	usage(void);
+
+#endif
--- src/parsing/tokenizer.c
+++ src/parsing/tokenizer.c
@@ -6,40 +6,115 @@
 /*   By: almeekel <almeekel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 17:14:42 by almeekel          #+#    #+#             */
-/*   Updated: 2025/04/09 17:24:44 by almeekel         ###   ########.fr       */
+/*   Updated: 2025/04/09 18:25:28 by almeekel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-skip_spaces(char *str, int *i)
+#include "../includes/minishell.h"
+
+t_token_type	identify_operator(const char *s, int *i)
 {
-
-
-	
+	if (s[*i] == '|')
+	{
+		(*i)++;
+		return (TOKEN_PIPE);
+	}
+	if (s[*i] == '<')
+	{
+		if (s[*i + 1] == '<')
+		{
+			(*i) += 2;
+			return (TOKEN_HEREDOC);
+		}
+		(*i)++;
+		return (TOKEN_REDIRECT_IN);
+	}
+	if (s[*i] == '>')
+	{
+		if (s[*i + 1] == '>')
+		{
+			(*i) += 2;
+			return (TOKEN_APPEND);
+		}
+		(*i)++;
+		return (TOKEN_REDIRECT_OUT);
+	}
+	return (-1);
 }
 
-// find |, <, >, >>, << //
-identify_operator(char *str, int *i)
+char	*identify_word(const char *str, int *i)
 {
-	if (str[*i]) == '|'
-	if (str[*i]) == '|'
-	if (str[*i]) == '|'
-	if (str[*i]) == '|'
+	int	start;
 
-
-	
+	start = *i;
+	while (str[*i] != '\0' && str[*i] != ' ' && str[*i] != '\t'
+		&& str[*i] != '|' && str[*i] != '<' && str[*i] != '>')
+	{
+		(*i)++;
+	}
+	return (strndup(&str[start], *i - start));
 }
-// finds words //
-identify_word(char *str, int *i)
+
+void	add_token(t_token **head, char *value, t_token_type type)
 {
-	
+	t_token	*new;
+	t_token	*tmp;
 
+	new = malloc(sizeof(t_token));
+	if (!new)
+		return ; // handle malloc fail later
+	new->value = value;
+	new->type = type;
+	new->next = NULL;
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		tmp = *head;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = new;
+	}
 }
-//adds the token to the list //
-add_token(t_token **head, char *val, t_token_type type)
-{}
 
 t_token	*tokenize(char *input)
 {
-	
-}
+	t_token			*tokens;
+	int				i;
+	t_token_type	type;
+	char			*op_value;
+	char			*word;
 
+	tokens = NULL;
+	i = 0;
+	while (input[i] != '\0')
+	{
+		while (input[i] == ' ' || input[i] == '\t')
+			i++;
+		if (input[i] == '\0')
+			break ;
+		if (input[i] == '|' || input[i] == '<' || input[i] == '>')
+		{
+			type = identify_operator(input, &i);
+			if (type == TOKEN_PIPE)
+				op_value = strdup("|");
+			else if (type == TOKEN_REDIRECT_IN)
+				op_value = strdup("<");
+			else if (type == TOKEN_REDIRECT_OUT)
+				op_value = strdup(">");
+			else if (type == TOKEN_APPEND)
+				op_value = strdup(">>");
+			else if (type == TOKEN_HEREDOC)
+				op_value = strdup("<<");
+			else
+				continue ;
+			add_token(&tokens, op_value, type);
+		}
+		else
+		{
+			word = identify_word(input, &i);
+			add_token(&tokens, word, TOKEN_WORD);
+		}
+	}
+	return (tokens);
+}
