1723bb0eef9cfd74925ff7d80e9bd5d97b03c7d4
Alexandre Meekel-Konjikovic <almeekel@z4r12p1.42lyon.fr>
2025-06-19 19:09:18 +0200

    working minishell

--- obj/src/signal/signal_handler.d
+++
@@ -1,9 +0,0 @@
-obj/src/signal/signal_handler.o: src/signal/signal_handler.c \
-  src/signal/../../includes/signals.h src/signal/../../includes/struct.h \
-  src/signal/../../includes/../libft/all.h
-
-src/signal/../../includes/signals.h:
-
-src/signal/../../includes/struct.h:
-
-src/signal/../../includes/../libft/all.h:
--- obj/src/utils/find_first.d
+++
@@ -1,9 +0,0 @@
-obj/src/utils/find_first.o: src/utils/find_first.c \
-  libft/../includes/utils.h libft/../includes/struct.h \
-  libft/../includes/../libft/all.h
-
-libft/../includes/utils.h:
-
-libft/../includes/struct.h:
-
-libft/../includes/../libft/all.h:
